1c97ff12998f40a17b5112132cf5ba49c1908729
Vilas Patil <patildipak422@gmail.com>
2021-02-28 23:18:00 +0530

    Studying Variables

---
+++ variables.js
@@ -0,0 +1,10 @@
+let name = 'Vilas Patil'
+
+let score = 100.20
+let bonus = 20
+let totalScore = score + bonus
+console.log(name);
+console.log(totalScore);
+let firstName = 'Vilas'
+let lastName = 'Patil'
+console.log(firstName + ' ' + lastName)
